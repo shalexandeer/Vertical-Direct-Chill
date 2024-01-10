207c4cf598539a946dad758b06b620447043b0c8
shalexandeer <shalexander09@gmail.com>
2024-01-10 19:16:59 +0700

    Update edit.blade.php

--- joki-wad/resources/views/billet/edit.blade.php
+++ joki-wad/resources/views/billet/edit.blade.php
@@ -43,7 +43,7 @@
               Billet</label>
             <select id="status" name="status" value="{{ $billet->status }}"
               class="form-control bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option value="jalan">Sedang Berjalan</option>
+              <option selected="" value="jalan">Sedang Berjalan</option>
               <option value="cacat">Ditemukan Kecacatan</option>
               <option value="selesai">Selesai</option>
             </select>
@@ -66,7 +66,7 @@
           </div>
         </div>
         <button type="submit"
-          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Tambahkan</button>
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</button>
       </form>
     </div>
   </div>
